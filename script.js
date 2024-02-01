b23b8031923e3a39448f064db4954e9a38dd65ae
Sergiu <sergiu.balauca@gmail.com>
2024-02-01 19:59:34 +0200

    template for terms and conditions page

---
+++ script.js
@@ -0,0 +1,3 @@
+// You can add JavaScript functionality if needed
+// For example, tracking user interactions or displaying dynamic content.
+// This script file is optional based on your requirements.
